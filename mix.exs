c3a033e8490dc7f57552c4ec8b4076d0e74f41e6
nelsonic <nelson+github@dwyl.io>
2020-05-30 11:04:14 +0100

    add package details to mix.exs

--- mix.exs
+++ mix.exs
@@ -4,10 +4,29 @@ defmodule Ping.MixProject do
   def project do
     [
       app: :ping,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: "Content Negotation lets you render HTML and JSON in the same route.",
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
+  end
+
+  defp package() do
+    [
+      files: ~w(lib/ping.ex LICENSE mix.exs README.md),
+      name: "ping",
+      licenses: ["GNU GPL v2.0"],
+      maintainers: ["dwyl"],
+      links: %{"GitHub" => "https://github.com/dwyl/ping"}
     ]
   end
 
@@ -21,8 +40,18 @@ defmodule Ping.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # Plug helper functions: github.com/elixir-plug/plug
+      {:plug, "~> 1.10", only: [:dev, :test]},
+
+      # Testing Wildcard Route Handler in a Phoenix App:
+      {:jason, "~> 1.2.0", only: [:dev, :test]},
+      {:phoenix, "~> 1.5.3", only: [:dev, :test]},
+
+      # Track coverage: github.com/parroty/excoveralls
+      {:excoveralls, "~> 0.12.3", only: :test},
+
+      # For publishing Hex.docs:
+      {:ex_doc, "~> 0.21.3", only: :dev}
     ]
   end
 end
