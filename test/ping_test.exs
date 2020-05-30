e6f0eee56706ed16cc44b116e244e79df29a6c70
nelsonic <nelson+github@dwyl.io>
2020-05-30 17:00:56 +0100

    create mini phoenix app to test render_pixel/2 #1

--- test/ping_test.exs
+++ test/ping_test.exs
@@ -1,8 +1,43 @@
 defmodule PingTest do
-  use ExUnit.Case
-  doctest Ping
+  @moduledoc """
+    If you want to understand how this test is set up, read: https://git.io/JfrZq
+    That is where I discovered that I could in-line an entire Phoenix App
+    in a single test file.
+    """
+    use ExUnit.Case, async: true
+    use Plug.Test
 
-  test "greets the world" do
-    assert Ping.hello() == :world
-  end
+    # Borrowed from: https://git.io/JfrZt
+    def call(router, verb, path, params \\ nil) do
+      verb
+      |> conn(path, params)
+      |> router.call(router.init([]))
+    end
+
+    defmodule Controller do
+      use Phoenix.Controller
+
+      def ping(conn, params), do: Ping.render_pixel(conn, params)
+    end
+
+    defmodule Router do
+      use Phoenix.Router
+
+      pipeline :any do
+        plug :accepts, ~w(html)
+      end
+
+      scope "/" do
+        pipe_through :any
+
+        get "/ping", Controller, :ping
+      end
+    end
+
+    test "routes to :ping and renders 1x1px GIF" do
+      conn = call(Router, :get, "ping")
+      assert conn.status == 200
+      assert conn.state == :sent
+      assert conn.resp_body =~ <<71, 73, 70, 56, 57>>
+    end
 end
