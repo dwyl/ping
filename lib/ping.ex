513ec6a6a18009ab315947163d89e93ff72ef70b
nelsonic <nelson+github@dwyl.io>
2020-05-30 20:22:49 +0100

    mix format

--- lib/ping.ex
+++ lib/ping.ex
@@ -3,8 +3,8 @@ defmodule Ping do
 
   # This is the Binary encoding for a 1x1 transparent pixel GIF
   # stackoverflow.com/questions/4665960/most-efficient-way-to-display-a-1x1-gif
-  @img <<71, 73, 70, 56, 57, 97, 1, 0, 1, 0, 128, 0, 0, 255, 255, 255, 0, 0, 0,
-  33, 249, 4, 1, 0, 0, 0, 0, 44, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 2, 68, 1, 0, 59>>
+  @img <<71, 73, 70, 56, 57, 97, 1, 0, 1, 0, 128, 0, 0, 255, 255, 255, 0, 0, 0, 33, 249, 4, 1, 0,
+         0, 0, 0, 44, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 2, 68, 1, 0, 59>>
 
   # apparently adding the "content-length" header speeds response time by 10%!
   @size byte_size(@img) |> to_string
@@ -13,7 +13,8 @@ defmodule Ping do
   `render_pixel/2` renders a 1x1 pixel GIF with appropriate headers.
   """
   def render_pixel(conn, _params) do
-    conn # instruct browser not to cache the image
+    # instruct browser not to cache the image
+    conn
     |> put_resp_header("cache-control", "no-store, private")
     |> put_resp_header("pragma", "no-cache")
     |> put_resp_content_type("image/gif")
