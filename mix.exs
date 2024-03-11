7e513eb61034f9743eed4e5eef4081243eeb423a
Luís Arteiro <lucho14garfield@gmail.com>
2024-03-11 16:36:36 +0000

    fix: Upgrading deps so mix compiles.

--- mix.exs
+++ mix.exs
@@ -41,11 +41,11 @@ defmodule Ping.MixProject do
   defp deps do
     [
       # Plug helper functions: github.com/elixir-plug/plug
-      {:plug, "~> 1.12.1"},
+      {:plug, "~> 1.15.3"},
 
       # Testing Wildcard Route Handler in a Phoenix App:
-      {:jason, "~> 1.2.2", only: [:dev, :test]},
-      {:phoenix, "~> 1.6.0", only: [:dev, :test]},
+      {:jason, "~> 1.4.1", only: [:dev, :test]},
+      {:phoenix, "~> 1.7.11", only: [:dev, :test]},
 
       # Track coverage: github.com/parroty/excoveralls
       {:excoveralls, "~> 0.14.3", only: :test},
