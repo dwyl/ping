97240d52c965ab9764a65aae246de1baeceebf1a
nelsonic <nelson+github@dwyl.com>
2024-03-13 06:20:11 +0000

    update version to v2 for shift to Fly.io #7 #8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ping.MixProject do
   def project do
     [
       app: :ping,
-      version: "1.1.0",
+      version: "2.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
